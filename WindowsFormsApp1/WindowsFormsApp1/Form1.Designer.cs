d2170ae73098d5aabb7569ee86a104fd6df97a8c
Kuzmin_Svyatoslav <svitosl@mail.ru>
2021-04-12 19:35:14 +0300

    added new Numbers application

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_TRNSLT = new System.Windows.Forms.Button();
+            this.Text_string = new System.Windows.Forms.TextBox();
+            this.Label_1 = new System.Windows.Forms.Label();
+            this.Label_2 = new System.Windows.Forms.Label();
+            this.Text_Numbers = new System.Windows.Forms.TextBox();
+            this.Label_3 = new System.Windows.Forms.Label();
+            this.Text_Roman = new System.Windows.Forms.TextBox();
+            this.Text_errors = new System.Windows.Forms.TextBox();
+            this.Label_4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTN_TRNSLT
+            // 
+            this.BTN_TRNSLT.Location = new System.Drawing.Point(37, 390);
+            this.BTN_TRNSLT.Name = "BTN_TRNSLT";
+            this.BTN_TRNSLT.Size = new System.Drawing.Size(75, 23);
+            this.BTN_TRNSLT.TabIndex = 0;
+            this.BTN_TRNSLT.Text = "Перевести";
+            this.BTN_TRNSLT.UseVisualStyleBackColor = true;
+            this.BTN_TRNSLT.Click += new System.EventHandler(this.BTN_TRNSLT_Click);
+            // 
+            // Text_string
+            // 
+            this.Text_string.Location = new System.Drawing.Point(39, 69);
+            this.Text_string.Name = "Text_string";
+            this.Text_string.Size = new System.Drawing.Size(316, 20);
+            this.Text_string.TabIndex = 1;
+            // 
+            // Label_1
+            // 
+            this.Label_1.AutoSize = true;
+            this.Label_1.Location = new System.Drawing.Point(36, 53);
+            this.Label_1.Name = "Label_1";
+            this.Label_1.Size = new System.Drawing.Size(237, 13);
+            this.Label_1.TabIndex = 2;
+            this.Label_1.Text = "Число текстом на английском языке (0-999):";
+            // 
+            // Label_2
+            // 
+            this.Label_2.AutoSize = true;
+            this.Label_2.Location = new System.Drawing.Point(36, 108);
+            this.Label_2.Name = "Label_2";
+            this.Label_2.Size = new System.Drawing.Size(252, 13);
+            this.Label_2.TabIndex = 3;
+            this.Label_2.Text = "Число, преобразованное в цифровые значения:";
+            // 
+            // Text_Numbers
+            // 
+            this.Text_Numbers.Location = new System.Drawing.Point(37, 124);
+            this.Text_Numbers.Name = "Text_Numbers";
+            this.Text_Numbers.Size = new System.Drawing.Size(318, 20);
+            this.Text_Numbers.TabIndex = 4;
+            // 
+            // Label_3
+            // 
+            this.Label_3.AutoSize = true;
+            this.Label_3.Location = new System.Drawing.Point(36, 166);
+            this.Label_3.Name = "Label_3";
+            this.Label_3.Size = new System.Drawing.Size(231, 13);
+            this.Label_3.TabIndex = 5;
+            this.Label_3.Text = "Число, преобразованное в римские цифры:";
+            // 
+            // Text_Roman
+            // 
+            this.Text_Roman.Location = new System.Drawing.Point(39, 192);
+            this.Text_Roman.Name = "Text_Roman";
+            this.Text_Roman.Size = new System.Drawing.Size(316, 20);
+            this.Text_Roman.TabIndex = 6;
+            // 
+            // Text_errors
+            // 
+            this.Text_errors.Location = new System.Drawing.Point(35, 255);
+            this.Text_errors.Multiline = true;
+            this.Text_errors.Name = "Text_errors";
+            this.Text_errors.Size = new System.Drawing.Size(442, 95);
+            this.Text_errors.TabIndex = 7;
+            // 
+            // Label_4
+            // 
+            this.Label_4.AutoSize = true;
+            this.Label_4.Location = new System.Drawing.Point(36, 229);
+            this.Label_4.Name = "Label_4";
+            this.Label_4.Size = new System.Drawing.Size(50, 13);
+            this.Label_4.TabIndex = 8;
+            this.Label_4.Text = "Ошибки:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 450);
+            this.Controls.Add(this.Label_4);
+            this.Controls.Add(this.Text_errors);
+            this.Controls.Add(this.Text_Roman);
+            this.Controls.Add(this.Label_3);
+            this.Controls.Add(this.Text_Numbers);
+            this.Controls.Add(this.Label_2);
+            this.Controls.Add(this.Label_1);
+            this.Controls.Add(this.Text_string);
+            this.Controls.Add(this.BTN_TRNSLT);
+            this.Name = "Form1";
+            this.Text = "Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_TRNSLT;
+        private System.Windows.Forms.TextBox Text_string;
+        private System.Windows.Forms.Label Label_1;
+        private System.Windows.Forms.Label Label_2;
+        private System.Windows.Forms.TextBox Text_Numbers;
+        private System.Windows.Forms.Label Label_3;
+        private System.Windows.Forms.TextBox Text_Roman;
+        private System.Windows.Forms.TextBox Text_errors;
+        private System.Windows.Forms.Label Label_4;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Функции
+        // is_standart() - проверка на число 1-9
+        // is_non_standart() - проверка на число 10-19
+        // is_tens() - проверка на десятки 20-90
+        // check_next() - проверка, нет ли после корректно записанного числа в строке никаких других лишних записей
+        // check_previous() - проверка, не записано ли число одинакового разряда два раза подряд
+        // convert_to_roman() - перевод числа в римские цифры
+
+        // Массив control [i] - хранит информацию о том, какие разряды чисел были записаны на каждом шаге
+        // control[i] = 1 - предыдущее число 1-9
+        // control[i] = 2 - предыдущее число десятки 20-90
+        // control[i] = 3 - предыдущая запись hundred
+
+        public int is_standart(string type)
+        {
+            switch (type)
+            {
+                case "one":
+                    return 1;
+                case "two":
+                    return 2;
+                case "three":
+                    return 3;
+                case "four":
+                    return 4;
+                case "five":
+                    return 5;
+                case "six":
+                    return 6;
+                case "seven":
+                    return 7;
+                case "eight":
+                    return 8;
+                case "nine":
+                    return 9;
+                default:
+                return -1;
+
+            }
+        }
+
+        public int is_non_standart(string type)
+        {
+            switch (type)
+            {
+                case "ten":
+                    return 10;
+                case "eleven":
+                    return 11;
+                case "twelve":
+                    return 12;
+                case "thirteen":
+                    return 13;
+                case "fourteen":
+                    return 14;
+                case "fifteen":
+                    return 15;
+                case "sixteen":
+                    return 16;
+                case "seventeen":
+                    return 17;
+                case "eighteen":
+                    return 18;
+                case "nineteen":
+                    return 19;
+                 default:
+                    return -1;
+             }
+        }
+
+        public int is_tens(string type)
+        {
+            switch (type)
+            {
+                case "twenty":
+                    return 2;
+                case "thirty":
+                    return 3;
+                case "forty":
+                    return 4;
+                case "fifty":
+                    return 5;
+                case "sixty":
+                    return 6;
+                case "seventy":
+                    return 7;
+                case "eighty":
+                    return 8;
+                case "ninety":
+                    return 9;
+                default:
+                    return -1;
+            }
+            }
+
+        public void check_next(int i, ref string[] mas)
+        {
+            if (i < (mas.Length - 1))
+                Text_errors.Text = "После " + "'"+mas[i]+"' не может быть введено никаких записей.";
+        }
+
+        public bool check_previous(int previous, string current)
+        {
+            if ((is_standart(current) != -1) && (previous == 1) ||
+                (is_tens(current) != -1) && (previous == 2))
+            {
+                Text_errors.Text = "Не может быть введено подряд два числа одинакового разряда.";
+                return true;
+            }
+            else if ((current == "hundred") && (previous == 3))
+            {
+                Text_errors.Text = "Cлово 'hundred' не может быть введено два раза подряд.";
+                return true;
+            }
+            else return false;
+        }
+
+        public void convert_to_roman(int number)
+        {
+            int[] ar = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            string[] ro = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+            int i = 0;
+            string result = "";
+           
+            while (number > 0)
+            {
+                if (ar[i] <= number)
+                {
+                    number = number - ar[i];
+                    result += ro[i];
+                }
+                else i++;
+            }
+            Text_Roman.Text = result;
+        }
+
+        private void BTN_TRNSLT_Click(object sender, EventArgs e)
+        {
+            Text_string.Text = Text_string.Text.ToLower();
+            Text_Numbers.Text = "";
+            Text_Roman.Text = "";
+            Text_errors.Text = "";
+
+            int i = 0;
+            bool cycle = true;
+            bool hundred = false;
+            string tmp = null;
+            string[] check = Text_string.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] control = new int[check.Length];
+
+            while (cycle)
+            { cycle = false;
+
+                if (check.Length == 0) // Начало шага 1
+                {
+                    Text_errors.Text = "Число не введено.";
+                    return;
+                }
+                else if (check[i] == "zero")
+                {
+                    tmp = "0";
+                    Text_Numbers.Text = tmp;
+                    check_next(i, ref check);
+                    return;
+                }
+                else if (is_standart(check[i]) != -1)
+                {
+                    tmp = is_standart(check[i]).ToString();
+                    control[i] = 1;
+                }
+                else if (is_non_standart(check[i]) != -1)
+                {
+                    tmp = is_non_standart(check[i]).ToString();
+                    Text_Numbers.Text = tmp;
+                    check_next(i, ref check);
+                    break;
+                }
+                else if (is_tens(check[i]) != -1)
+                {
+                    tmp = is_tens(check[i]).ToString();
+                    control[i] = 2;
+                }
+                else
+                {
+                    Text_errors.Text = "Число " + "'" + check[i] + "' введено неправильно.";
+                    return;
+                }
+
+                if (i < (check.Length - 1)) // Начало шага 2
+                {
+                    i++;
+                    if (check_previous(control[i - 1], check[i])) return;
+                    else if ((is_standart(check[i - 1]) != -1) && (is_tens(check[i]) != -1))
+                    {
+                        Text_errors.Text = "В записи после числа единичного формата не может быть числа десятичного формата.";
+                        return;
+                    }
+                    else if ((is_tens(check[i - 1]) != -1) && (check[i] == "hundred"))
+                    {
+                        Text_errors.Text = "После числа десятичного формата не должно стоять слово 'hundred'.";
+                        return;
+                    }
+                    else if (check[i] == "hundred")
+                    {
+                        hundred = true;
+                        control[i] = 3;
+                    }
+                    else if (is_standart(check[i]) != -1)
+                    {
+                        tmp += is_standart(check[i]).ToString();
+                        Text_Numbers.Text = tmp;
+                        check_next(i, ref check);
+                        break;
+                    }
+                    else
+                    {
+                        if (control[i - 1] == 1)
+                        {
+                            Text_errors.Text = "Число " + "'" + check[i] + "' введено неправильно. Если это трехзначное число, то после числа единичного формата должно стоять слово 'hundred'.";
+                            return;
+                        }
+                        else if (control[i - 1] == 2)
+                        {
+                            Text_errors.Text = "Число " + "'" + check[i] + "' введено неправильно. Если это двузначное число, состоящее из двух слов, то после числа десятичного формата должно стоять число единичного формата.";
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    if (is_standart(check[i]) != -1)
+                    {
+                        Text_Numbers.Text = tmp;
+                        break;
+                    }
+                    else if (is_tens(check[i]) != -1)
+                    {
+                        tmp += "0";
+                        Text_Numbers.Text = tmp;
+                        break;
+                    }
+                }
+
+                if (i < (check.Length - 1)) // Начало шага 3
+                {
+                    i++;
+                    if (check_previous(control[i - 1], check[i])) return;
+                    else if (is_standart(check[i]) != -1)
+                    {
+                        tmp += "0";
+                        tmp += is_standart(check[i]).ToString();
+                        Text_Numbers.Text = tmp;
+                        check_next(i, ref check);
+                        break;
+                    }
+                    else if (is_non_standart(check[i]) != -1)
+                    {
+                        tmp += is_non_standart(check[i]).ToString();
+                        Text_Numbers.Text = tmp;
+                        check_next(i, ref check);
+                        break;
+                    }
+                    else if (is_tens(check[i]) != -1)
+                    {
+                        tmp += is_tens(check[i]).ToString();
+                        control[i] = 2;
+                    }
+                    else
+                    {
+                        Text_errors.Text = "Число" + "'" + check[i] + "' введено неправильно. Если это трехзначное число, состоящее из 3-ех или 4-ех слов, то после слова 'hundred' должно быть записано либо число единичного формата, либо число десятичного формата.";
+                        return;
+                    }
+                }
+                else
+                {
+                    if (hundred)
+                    {
+                        tmp += "00";
+                        Text_Numbers.Text = tmp;
+                        break;
+                    }
+                }
+
+                if (i < (check.Length - 1)) // Начало шага 4
+                {
+                    i++;
+                    if (check_previous(control[i - 1], check[i])) return;
+                    else if (is_standart(check[i]) != -1)
+                    {
+                        tmp += is_standart(check[i]).ToString();
+                        Text_Numbers.Text = tmp;
+                        check_next(i, ref check);
+                        break;
+                    }
+                    else
+                    {
+                        Text_errors.Text = "Число" + "'" + check[i] + "' введено неправильно. Если это трехзначное число, состоящее из четырех слов, то после числа десятичного формата должно быть записано число единичного формата.";
+                        return;
+                    }
+                }
+                else
+                {
+                    if (is_tens(check[i]) != -1)
+                    {
+                        tmp += "0";
+                        Text_Numbers.Text = tmp;
+                        break;
+                    }
+                }
+            }
+
+            convert_to_roman(Convert.ToInt32(tmp));
+        }
+    }
+}
